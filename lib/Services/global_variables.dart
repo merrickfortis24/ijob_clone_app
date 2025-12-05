60b0a8cf2a5fb878b9e9342c245f663b2dd13ed0
merrickfortis24 <fortismerrick@gmail.com>
2025-12-05 08:03:44 +0800

    Add animated background image to SignUp screen and introduce global variable for image URL

---
+++ lib/Services/global_variables.dart
@@ -0,0 +1,3 @@
+// filepath: lib/Services/global_variables.dart
+String signupUrlImage =
+    'https://assets.publishing.service.gov.uk/media/5a60f4f840f0b65266e7adbe/Cover.jpg';
--- lib/SignupPage/signup_screen.dart
+++ lib/SignupPage/signup_screen.dart
@@ -1,5 +1,7 @@
 // filepath: lib/SignupPage/signup_screen.dart
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import '../Services/global_variables.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -8,12 +10,68 @@ class SignUp extends StatefulWidget {
   State<SignUp> createState() => _SignUpState();
 }
 
-class _SignUpState extends State<SignUp> {
+class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
+  late Animation<double> _animation;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 20),
+    );
+
+    _animation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(
+            CurvedAnimation(parent: _animationController, curve: Curves.linear),
+          )
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              _animationController.reset();
+              _animationController.forward();
+            }
+          });
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Sign Up')),
-      body: const Center(child: Text('Signup screen')),
+      body: Stack(
+        children: [
+          // Background image loaded from network with placeholder
+          CachedNetworkImage(
+            imageUrl: signupUrlImage,
+            imageBuilder: (context, imageProvider) => Image(
+              image: imageProvider,
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+              alignment: FractionalOffset(_animation.value, 0),
+            ),
+            placeholder: (context, url) =>
+                Image.asset('assets/images/wallpaper.jpg', fit: BoxFit.fill),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            color: Colors.black54,
+          ),
+        ],
+      ),
     );
   }
 }
