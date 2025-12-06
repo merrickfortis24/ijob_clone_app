0bd37c246616cd720c321292415a8869ea145722
merrickfortis24 <fortismerrick@gmail.com>
2025-12-06 09:59:17 +0800

    hghh

--- lib/ForgetPassword/forget_password_screen.dart
+++ lib/ForgetPassword/forget_password_screen.dart
@@ -1,5 +1,8 @@
 // filepath: lib/ForgetPassword/forget_password_screen.dart
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ijob_clone_app/Services/global_variables.dart';
 
 class ForgetPassword extends StatefulWidget {
   const ForgetPassword({super.key});
@@ -8,12 +11,70 @@ class ForgetPassword extends StatefulWidget {
   State<ForgetPassword> createState() => _ForgetPasswordState();
 }
 
-class _ForgetPasswordState extends State<ForgetPassword> {
+class _ForgetPasswordState extends State<ForgetPassword>
+    with TickerProviderStateMixin {
+  // ignore: unused_field
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _forgetpassTextController = TextEditingController(
+    text: '',
+  );
+
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 20),
+    );
+    _animation =
+        CurvedAnimation(parent: _animationController, curve: Curves.linear)
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((animationStatus) {
+            if (animationStatus == AnimationStatus.completed) {
+              _animationController.reset();
+              _animationController.forward();
+            }
+          });
+    _animationController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _forgetpassTextController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Forget Password')),
-      body: const Center(child: Text('Forget password screen')),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: CachedNetworkImage(
+              imageUrl: forgetUrlImage,
+              imageBuilder: (context, imageProvider) => Image(
+                image: imageProvider,
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+                alignment: FractionalOffset(_animation.value, 0),
+              ),
+              placeholder: (context, url) =>
+                  Image.asset('assets/images/wallpaper.jpg', fit: BoxFit.fill),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/Services/global_variables.dart
+++ lib/Services/global_variables.dart
@@ -1,3 +1,5 @@
 // filepath: lib/Services/global_variables.dart
 String signupUrlImage =
     'https://assets.publishing.service.gov.uk/media/5a60f4f840f0b65266e7adbe/Cover.jpg';
+String forgetUrlImage =
+    'https://images.unsplash.com/photo-1508780709619-79562169bc64?auto=format&fit=crop&w=1350&q=80';
